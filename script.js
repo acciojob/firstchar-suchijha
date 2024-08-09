36fa7ad875887d2010a5069605d2381111720136
suchijha <94467537+suchijha@users.noreply.github.com>
2024-08-09 09:28:02 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-function firstChar(text) {
-  return text[0];
+function firstChar(str) {
+   const trimmedStr = str.trim();
+    
+    // Check if the trimmed string is empty
+    if (trimmedStr.length === 0) {
+        return ''; // Return empty string if no non-space characters found
+    }
+    
+    // Return the first character of the trimmed string
+    return trimmedStr.charAt(0);
 }
 
 // Do not change the code below
 //Uncomment the following line to show the prompt popup
-const text = prompt("Enter text:");
+//const text = prompt("Enter text:");
 alert(firstChar(text));
